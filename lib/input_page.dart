44bbc82aea958cb42c368ba2ffd00af61e7ae235
Paul Slinkard <slinkardpaul50@gmail.com>
2024-12-28 08:12:37 -0600

    Extracted custom widgets to their own files and then imported those files back into input_page.dart

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'icon_content.dart';
+import 'reusable_card.dart';
 
 const bottomContainerHeight = 80.0;
 const cardColor = Color(0xFF1D1E33);
